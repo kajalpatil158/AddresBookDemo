fcf7d4b486a31817504e57591fcfc5ba722cd953
Kajal <kajalpatil158@gmail.com>
2021-04-02 19:44:21 +0530

    [Kajal Patil]Added:Well Come Message

---
+++ UserRegistration.java
@@ -0,0 +1,10 @@
+package com.userregistration;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		System.out.println("Well Come In User Registration Program...");
+
+	}
+
+}
